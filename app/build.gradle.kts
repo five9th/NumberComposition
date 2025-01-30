3d88434dbb2c19f02b2565e55e5b362179deb239
five9th <yegorbalamutin1@gmail.com>
2025-01-30 18:19:55 +0300

    compileSdk changed to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.five9th.numbercomposition"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.five9th.numbercomposition"
