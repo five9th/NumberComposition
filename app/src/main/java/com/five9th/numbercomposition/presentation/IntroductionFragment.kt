9368901a07eecf0defe9fef24a06cbc10db37922
five9th <yegorbalamutin1@gmail.com>
2025-02-11 12:26:01 +0300

    binding added to IntroductionFragment

--- app/src/main/java/com/five9th/numbercomposition/presentation/IntroductionFragment.kt
+++ app/src/main/java/com/five9th/numbercomposition/presentation/IntroductionFragment.kt
@@ -1,6 +1,37 @@
 package com.five9th.numbercomposition.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.five9th.numbercomposition.databinding.FragmentIntroductionBinding
 
 class IntroductionFragment : Fragment() {
+
+    private var _binding: FragmentIntroductionBinding? = null
+    private val binding: FragmentIntroductionBinding
+        get() = _binding ?: throw RuntimeException("FragmentIntroductionBinding is null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentIntroductionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.startBtn.setOnClickListener {
+            //todo
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
