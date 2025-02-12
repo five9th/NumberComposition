fdcdadba4b5ae1569be738b3e1912c65896cb64a
five9th <yegorbalamutin1@gmail.com>
2025-02-12 14:04:00 +0300

    IntroductionFragment inherited from BaseFragmentV2

--- app/src/main/java/com/five9th/numbercomposition/presentation/IntroductionFragment.kt
+++ app/src/main/java/com/five9th/numbercomposition/presentation/IntroductionFragment.kt
@@ -4,23 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.five9th.numbercomposition.databinding.FragmentIntroductionBinding
 
-class IntroductionFragment : Fragment() {
-
-    private var _binding: FragmentIntroductionBinding? = null
-    private val binding: FragmentIntroductionBinding
-        get() = _binding ?: throw RuntimeException("FragmentIntroductionBinding is null")
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentIntroductionBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+class IntroductionFragment : BaseFragmentV2<FragmentIntroductionBinding>(
+    FragmentIntroductionBinding::inflate
+) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,10 +16,7 @@ class IntroductionFragment : Fragment() {
         binding.startBtn.setOnClickListener {
             //todo
         }
+        binding.startBtn.text = "Hello from binding"
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
