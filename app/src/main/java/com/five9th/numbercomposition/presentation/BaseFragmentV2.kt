9aad4ebd6894456d6ac64130d238e87052aaeaa5
five9th <yegorbalamutin1@gmail.com>
2025-02-12 13:54:45 +0300

    BaseFragmentV2 created

---
+++ app/src/main/java/com/five9th/numbercomposition/presentation/BaseFragmentV2.kt
@@ -0,0 +1,33 @@
+package com.five9th.numbercomposition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragmentV2<VB : ViewBinding>(
+    private val inflate: (LayoutInflater, ViewGroup?, Boolean) -> VB
+) : Fragment() {
+
+    private var _binding: VB? = null
+    protected val binding: VB
+        get() {
+            return _binding ?: throw IllegalStateException("ViewBinding is null")
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflate(inflater, container, false)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
