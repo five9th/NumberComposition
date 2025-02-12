25796bdc9db129f8aa482bc1bee024cd35a79d0f
five9th <yegorbalamutin1@gmail.com>
2025-02-12 13:13:52 +0300

    BaseFragment created

---
+++ app/src/main/java/com/five9th/numbercomposition/presentation/BaseFragment.kt
@@ -0,0 +1,30 @@
+package com.five9th.numbercomposition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB: ViewBinding> : Fragment() {
+    private var _binding: VB? = null
+    protected val binding: VB
+        get() = _binding ?: throw RuntimeException("ViewBinding is null")
+
+    abstract fun inflateBinding(inflater: LayoutInflater, container: ViewGroup?): VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = inflateBinding(inflater, container)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
